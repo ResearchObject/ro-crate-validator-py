f4a0cfd4272d85f1af053df489eb97c34a342f19
Stian Soiland-Reyes <soiland-reyes@manchester.ac.uk>
2023-03-05 23:38:50 +0000

    include dependencies

--- src/setup.py
+++ src/setup.py
@@ -8,6 +8,7 @@ setuptools.setup(name = 'rocrateValidator',
 			],
 	package_data = {"rocrateValidator": ["rocrateValidator/workflow_extension.txt", "rocrateValidator/check_list.txt"]},
   	include_package_data=True,
+	install_requires=["requests>=2", "rocrate>=0.7"],
 	packages=setuptools.find_packages(),
 	url = "https://github.com/ResearchObject/ro-crate-validator-py",
 	zip_safe = False
